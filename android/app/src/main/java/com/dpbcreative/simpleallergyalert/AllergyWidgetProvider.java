c66a6b14a23fc0d811fe50c7387c93f57eb5d68c
[dyad] <dbuzugbe@gmail.com>
2026-03-12 14:10:48 +0700

    [dyad] Updating widget UI and adding refresh functionality - wrote 2 file(s) + extra files edited outside of Dyad

--- android/app/src/main/java/com/dpbcreative/simpleallergyalert/AllergyWidgetProvider.java
+++ android/app/src/main/java/com/dpbcreative/simpleallergyalert/AllergyWidgetProvider.java
@@ -12,6 +12,7 @@ import android.widget.RemoteViews;
 public class AllergyWidgetProvider extends AppWidgetProvider {
 
     public static final String ACTION_OPEN_CARD = "com.dpbcreative.simpleallergyalert.ACTION_OPEN_CARD";
+    public static final String ACTION_REFRESH = "com.dpbcreative.simpleallergyalert.ACTION_REFRESH";
     public static final String EXTRA_CARD_ID = "com.dpbcreative.simpleallergyalert.EXTRA_CARD_ID";
 
     @Override
@@ -28,7 +29,14 @@ public class AllergyWidgetProvider extends AppWidgetProvider {
         // Set up the intent for the Emergency button
         Intent emergencyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("simpleallergyalert://emergency"));
         PendingIntent emergencyPendingIntent = PendingIntent.getActivity(context, 0, emergencyIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-        views.setOnClickPendingIntent(R.id.emergency_button, emergencyPendingIntent);
+        views.setOnClickPendingIntent(R.id.emergency_container, emergencyPendingIntent);
+
+        // Set up the intent for the Refresh button
+        Intent refreshIntent = new Intent(context, AllergyWidgetProvider.class);
+        refreshIntent.setAction(ACTION_REFRESH);
+        refreshIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        PendingIntent refreshPendingIntent = PendingIntent.getBroadcast(context, appWidgetId, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        views.setOnClickPendingIntent(R.id.refresh_button, refreshPendingIntent);
 
         // Set up the intent for the footer (open app)
         Intent mainIntent = new Intent(context, MainActivity.class);
@@ -53,12 +61,25 @@ public class AllergyWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        
         if (ACTION_OPEN_CARD.equals(intent.getAction())) {
             String cardId = intent.getStringExtra(EXTRA_CARD_ID);
             if (cardId != null) {
                 Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("simpleallergyalert://card/" + cardId));
                 appIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(appIntent);
+            }
+        } else if (ACTION_REFRESH.equals(intent.getAction())) {
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+            if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+                // Notify the ListView to refresh its data
+                appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.card_list);
+            } else {
+                // Refresh all widgets if ID is invalid
+                ComponentName componentName = new ComponentName(context, AllergyWidgetProvider.class);
+                int[] ids = appWidgetManager.getAppWidgetIds(componentName);
+                appWidgetManager.notifyAppWidgetViewDataChanged(ids, R.id.card_list);
             }
         }
         super.onReceive(context, intent);
